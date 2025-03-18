5cbe1fbe307165f322e6b0cbf45af69835e37a32
nareknaka <narek.grigoryan1993@gmail.com>
2025-03-18 21:51:02 +0400

    added final project

---
+++ Variables/B-N-Generator.py
@@ -0,0 +1,11 @@
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be: "+ city + " "+ pet)
+
+# The input() function is used to collect user input.
+# The input() function will display a message to the user asking for the city they grew up in.
+# The input() function will take the user input and store it in the variable city.
+# The input() function will display a message to the user asking for the name of a pet.
+# The input() function will take the user input and store it in the variable pet.
+# The print() function will display the band name to the user.
