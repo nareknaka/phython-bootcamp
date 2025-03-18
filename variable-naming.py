6f8b32d36f0b0bbc4786e900784e3121dc11786d
nareknaka <narek.grigoryan1993@gmail.com>
2025-03-18 21:36:11 +0400

    added variable name

---
+++ variable-naming.py
@@ -0,0 +1,7 @@
+name = "Angela"
+length = len(name)
+print(length)
+
+# The variable name is a string variable that stores the value "Angela".
+# The variable length is an integer variable that stores the length of the string variable name.
+# The len() function is used to calculate the length of the string variable name.
