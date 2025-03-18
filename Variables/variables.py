55b6aeb105cb1c69c493d1f6af49dc882a399b24
nareknaka <narek.grigoryan1993@gmail.com>
2025-03-18 21:23:11 +0400

    Narek/changed stroke

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,10 +1,10 @@
-# print(len(input("What is your name?")))
 name="Jack"
 print(name)
 
 name="Angela"
 print(name)
 
+# print(len(input("What is your name?")))
 username = input("what is your name?")
 length = len(username)
 print(length)
