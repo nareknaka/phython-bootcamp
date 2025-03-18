540530a512d98f88b13153aa96e08f46bd52c689
nareknaka <narek.grigoryan1993@gmail.com>
2025-03-18 20:43:46 +0400

    input funcion

--- Variables/inputs.py
+++ Variables/inputs.py
@@ -1,4 +1,6 @@
 # input() function to collect user input and use it within your code.
 # This line of code will take an input the input() function and store it in a variable called name.
 # The input() function will display a message to the user asking for their name.
+# The input() function will take the user input and store it in the variable name.
+
 print("hello " + input("what is your name?") + "!")
